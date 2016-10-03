a10a855834e5dbf89c307fdfa568cae41ec01f8d
Jason Axelson <jason.axelson@gmail.com>
2016-10-03 08:27:11 -1000

    remove unsafe logging

--- server/models/user.js
+++ server/models/user.js
@@ -6,14 +6,11 @@ const User = bookshelf.Model.extend({
   hasTimestamps: true,
 
   generateHash: function(password) {
-    console.log('generating hash of', password)
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
   },
 
   // checking if password is valid
   validPassword: function(password) {
-    console.log('passed in  password is: ', password)
-    console.log('db password is: ', this.attributes.password)
     return bcrypt.compareSync(password, this.attributes.password)
   },
 })
