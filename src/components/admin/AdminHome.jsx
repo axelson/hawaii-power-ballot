d594504f878dc49249483cdd7e1f24e6da8c5f62
Jason Axelson <jason.axelson@gmail.com>
2016-10-13 21:10:30 -1000

    Admin Home show number of filled values

--- src/components/admin/AdminHome.jsx
+++ src/components/admin/AdminHome.jsx
@@ -1,20 +1,41 @@
 import React, { PropTypes } from 'react'
 
+import styles from './admin-home.scss'
+
 export default class AdminHome extends React.Component {
+  _renderEditProgress (candidate) {
+    if (!candidate) return null
+
+    const metadata = candidate.metadata
+    const totalNumFields = Object.keys(metadata).length
+    const filledValues = Object.keys(metadata).filter((key) => {
+      !!metadata[key]
+    }).length
+
+    return (
+      <span>
+        &nbsp;{filledValues}/{totalNumFields}
+      </span>
+    )
+  }
+
+  _renderCandidate = (candidate) => {
+    return (
+      <div key={candidate.Candidate_ID}>
+        {candidate.Candidate_Name}&nbsp;
+        (<a href={`/admin/candidate/${candidate.Candidate_ID}`}>Edit</a>)
+        {this._renderEditProgress(candidate)}
+      </div>
+    )
+  }
+
   render () {
     const { candidates } = this.props
 
     return (
-      <div>
-        AdminHome
-        {candidates.map((candidate) => {
-          return (
-            <div key={candidate.Candidate_ID}>
-              {candidate.Candidate_Name}&nbsp;
-              (<a href={`/admin/candidate/${candidate.Candidate_ID}`}>Edit</a>)
-            </div>
-          )
-        })}
+      <div className={styles['container']}>
+        <h1>Admin Home Page</h1>
+        {candidates.map(this._renderCandidate)}
       </div>
     )
   }
