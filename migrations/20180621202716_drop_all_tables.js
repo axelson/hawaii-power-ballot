84235c7d5cb9ddac5fbbdb1567830703025d80d6
Jason Axelson <jason.axelson@gmail.com>
2018-06-21 20:47:51 -1000

    migrations: drop all candidate-related tables

---
+++ migrations/20180621202716_drop_all_tables.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('candidate_metadata'),
+    knex.schema.dropTable('candidates'),
+    knex.schema.dropTable('polling_places')
+  ])
+}
+
+exports.down = function(_knex, _Promise) {
+}
