cade3fb9e26dc71eb8d162bb2d713bdbf9e11275
Jason Axelson <jason.axelson@gmail.com>
2018-06-21 22:46:20 -1000

    linting fix

--- migrations/20180621202716_drop_all_tables.js
+++ migrations/20180621202716_drop_all_tables.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.dropTable('candidate_metadata'),
     knex.schema.dropTable('candidates'),
-    knex.schema.dropTable('polling_places')
+    knex.schema.dropTable('polling_places'),
   ])
 }
 
