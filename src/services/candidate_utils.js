c22cc1786e25d6b2b5b92c4920893ea61567b3b2
Jason Axelson <jason.axelson@gmail.com>
2018-07-14 17:58:15 -1000

    Update candidate link helpers

--- src/services/candidate_utils.js
+++ src/services/candidate_utils.js
@@ -48,84 +48,42 @@ export function metadataFieldNameToTitle(fieldName) {
   switch (fieldName) {
 
   // General
-  case 'ballot_name':
+  case 'candidate_name':
     return 'Ballot Name'
-  case 'Office':
-    return 'Office'
-  case 'District':
-    return 'District'
-  case 'Party':
-    return 'Party'
-  case 'name_party':
+  case 'contests':
+    return 'Contests'
+  case 'party':
     return 'Name of Party'
-  case 'office_district':
-    return 'Office District'
-  case 'incumbent_text':
-    return 'Incumbent Text'
-  case 'photo_url':
+  case 'candidate_photo_url':
     return 'Photo URL'
   case 'photo_width':
     return 'Photo Width'
   case 'photo_source_text':
     return 'Photo Alt Text'
-  case 'party_text':
-    return 'Party Text'
-  case 'occupation':
-    return 'Occupation'
-  case 'cc_name':
-    return 'Candidate Committee Name'
-  case 'cc_chair':
-    return 'Candidate Committee Chair'
-  case 'cc_treasurer':
-    return 'Candidate Committee Treasurer'
-  case 'volunteer':
-    return 'Volunteer'
-  case 'bio_cred_url':
-    return 'Bio URL'
-  case 'bio_cred_text':
-    return 'Bio Text'
-  case 'bio':
-    return 'Biography'
-
-  // Links
-  case 'cc_report_url':
-    return 'Organizational Report'
-  case 'cand_website_url':
-    return 'Website'
-  case 'cand_email':
-    return 'Email'
-  case 'cand_facebook_url':
+  case 'phone':
+    return 'Phone number'
+  case 'website':
+    return 'Offical Website'
+  case 'facebook_url':
     return 'Facebook'
-  case 'cand_twitter_url':
+  case 'twitter_url':
     return 'Twitter'
-  case 'cand_linked':
-    return 'LinkedIn'
-  case 'csc_standard_url':
-    return 'Standard Reports'
-  case 'csc_special_url':
-    return 'Special Reports'
-  case 'fec_url':
-    return 'FEC URL'
-  case 'gov_website_url':
-    return 'Official Government Webpage'
-  case 'lwv_website_url':
-    return 'League of Women Voters of Hawaii'
-  case 'ftm_website_url':
-    return 'FollowTheMoney'
-  case 'pvs_website_url':
-    return 'Project Vote Smart'
-  case 'bp_website_url':
-    return 'Ballotpedia'
-  case 'wp_website_url':
-    return 'Wikipedia'
-  case 'cb_website_url':
-    return 'Civil Beat Q&A'
-  case 'pbs_hawaii_url':
-    return 'PBS Hawaii Insights'
-  case 'expenditure_viz_url':
-    return 'Campaign Expenditures Visualization'
+  case 'instagram_url':
+    return 'Instagram'
+  case 'email_address':
+    return 'Email Address'
+  case 'cc_registration_number':
+    return 'Candidate Committe Registration Number'
+  case 'fec_cc_id':
+    return 'FEC Candidate Committee Identifier'
+  case 'civil_beat_url':
+    return 'Civil Beat Link'
   case 'staradvertiser_url':
-    return 'Star Advertiser'
+    return 'Star-Advertiser Link'
+  case 'candidate_photo_url':
+    return 'Candidate Photo URL'
+  case 'contest_id':
+    return 'Contest Identifier'
   }
 
   return fieldName
