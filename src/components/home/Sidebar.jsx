78f30dc71ca10d8ad4c87e58caae32e6302015ab
Aaron Fujimoto <aaronfuj@gmail.com>
2018-07-14 16:25:53 -1000

    Added in primary election information

--- src/components/home/Sidebar.jsx
+++ src/components/home/Sidebar.jsx
@@ -3,15 +3,54 @@ import React from 'react'
 import styles from './sidebar.scss'
 
 export default function Sidebar() {
-  var t = Date.parse('2018-11-06') - Date.parse(new Date())
-  var daysTillGeneralElection = Math.floor( t/(1000*60*60*24) )
+  var primaryElectionDate = Date.parse('2018-08-11') - Date.parse(new Date())
+  var daysTillPrimaryElection = Math.floor( primaryElectionDate/(1000*60*60*24) )
+
+  var generalElectionDate = Date.parse('2018-11-06') - Date.parse(new Date())
+  var daysTillGeneralElection = Math.floor( generalElectionDate/(1000*60*60*24) )
 
   return (
     <div className={styles['container']}>
 
       <h3 className={styles['header']}>Key Dates</h3>
       <div className={styles['event-title']}>
-        Voter Registration Deadline
+        Primary Election Early Walk-in Voting/Late Registration
+      </div>
+      <p>
+        July 30 - August 9, 2018
+      </p>
+
+      <div className={styles['separator']} />
+
+      <div className={styles['event-title']}>
+        Primary Election Mail-In Ballot Deadline
+      </div>
+      <p>
+        August 4, 2018
+      </p>
+
+      <div className={styles['separator']} />
+
+      <div className={styles['general-election']}>
+        <div>
+          <div className={styles['event-title']}>
+            Primary Election
+          </div>
+          <p>
+            August 11, 2018
+            <br />
+            7am to 6pm
+          </p>
+        </div>
+        <div className={styles['countdown-container']}>
+          <div className={styles['countdown-number']}>{daysTillPrimaryElection}</div>
+          <div className={styles['countdown-days']}>days left</div>
+        </div>
+      </div>
+
+      <div className={styles['separator']} />
+      <div className={styles['event-title']}>
+        General Election Voter Registration Deadline
       </div>
       <p>
         October 9, 2018, 4:30pm
@@ -22,7 +61,7 @@ export default function Sidebar() {
       <div className={styles['separator']} />
 
       <div className={styles['event-title']}>
-        Early Walk-in Voting/Late Registration
+        General Election Early Walk-in Voting/Late Registration
       </div>
       <p>
         October 23 - November 3, 2018
@@ -31,7 +70,7 @@ export default function Sidebar() {
       <div className={styles['separator']} />
 
       <div className={styles['event-title']}>
-        Mail-In Ballot Deadline
+        General Election Mail-In Ballot Deadline
       </div>
       <p>
         October 30, 2018
@@ -47,7 +86,7 @@ export default function Sidebar() {
           <p>
             November 6, 2018
             <br />
-            7 am to 6pm
+            7am to 6pm
           </p>
         </div>
         <div className={styles['countdown-container']}>
