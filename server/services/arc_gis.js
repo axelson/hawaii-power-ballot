1e615d22604bf13865477fbf25109dfc2f78c2a4
Jason Axelson <jason.axelson@gmail.com>
2018-10-28 16:00:54 -1000

    Use statewide map service for address lookup

--- server/services/arc_gis.js
+++ server/services/arc_gis.js
@@ -110,8 +110,8 @@ function geocodeAddress (address) {
 // Use the precinct service to lookup an address based on "esri" coordinates
 function lookupPrecinct (coordinates, spatialReference) {
   // const baseUrl =  'https://services2.arcgis.com/tuFQUQg1xd48W6M5/ArcGIS/rest/services/HACC_HI2016G_Candidates/FeatureServer/1/query'
-
-  const baseUrl = 'https://services.arcgis.com/HQ0xoN0EzDPBOEci/arcgis/rest/services/Polling_Places_Honolulu/FeatureServer/5/query'
+  // const baseUrl = 'https://services.arcgis.com/HQ0xoN0EzDPBOEci/arcgis/rest/services/Polling_Places_Honolulu/FeatureServer/5/query'
+  const baseUrl = 'https://services.arcgis.com/HQ0xoN0EzDPBOEci/arcgis/rest/services/Polling_Places_Statewide/FeatureServer/5/query'
 
   const result = SuperAgent.get(baseUrl)
     .query({
